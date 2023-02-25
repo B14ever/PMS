e9402eab9109d6a24b2422db29980fd73186ef26
Edil-Feyeselgit config --global user.name Edil-Feysel <enebgirl18@gmail.com>
2023-02-26 00:06:51 +0300

    FixedIncome is fetching data

--- server/server.js
+++ server/server.js
@@ -253,6 +253,31 @@ app.get("/getRareIncome", async (req, res) => {
     });
   }
 });
+app.get("/getFixedIncome", async (req, res) => {
+  const { find } = req.query;
+  if (find) {
+    const sql = `SELECT * FROM received_property WHERE Received_Office_Name LIKE '%${find}%' AND Model = 'ሞዴል 19 ሀ'`;
+    con.query(sql, (err, data) => {
+      res.send(data);
+    });
+  } else {
+    const sql = `SELECT * FROM received_property WHERE Model = 'ሞዴል 19 ሀ'`;
+    con.query(sql, (err, data) => {
+      res.send(data);
+    });
+  }
+});
+app.post("/postNewFixedIncome", async (req, res) => {
+  const { PropertyType, Mesurment, Quantity, Price, Comment } = req.body;
+  const sql = `INSERT INTO received_property_details (Quantity,Unit_Cost,Property_Received_ID,RPD_Office_Name,Remark) VALUES ("${Quantity}","${Price}", "${PropertyType}","${Comment}")`;
+  con.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("successful");
+    }
+  });
+});
 app.post("/postNewRareIncome", async (req, res) => {
   const { PropertyType, Mesurment, Quantity, Price, Comment } = req.body;
   const sql = `INSERT INTO received_property_details (Quantity,Unit_Cost,Property_Received_ID,RPD_Office_Name,Remark) VALUES ("${Quantity}","${Price}", "${PropertyType}","${Comment}")`;
